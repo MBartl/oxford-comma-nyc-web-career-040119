41fe3ee054aaf8ddfa723d2dd535d1c939ba20d9
Matthew Bartl <mattbartl@gmail.com>
2019-03-20 00:51:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length > 2 do
+  if array.length > 2
     array.join(', ')
     array[-1] = 'and ' + array[-1]
-  elsif array.length == 2 do
+  elsif array.length == 2
     array[-1] = 'and ' + array[-1]
   end
 end
